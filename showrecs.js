9a57713054ed14634aee9525011c19780c9c4faf
Cyn18 <53236439+Cyn18@users.noreply.github.com>
2019-07-24 09:57:51 -0400

    Add files via upload

---
+++ showrecs.js
@@ -0,0 +1,6 @@
+function btn(){
+var btn = document.getElementById("BUTTON");   // Create a <button> element
+btn.innerHTML = "I rate this 8.5/10";
+            // Insert text
+document.body.appendChild(btn);               // Append <button> to <body>
+}
